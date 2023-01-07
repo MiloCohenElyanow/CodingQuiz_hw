3f6accb4050d1df52cdeeae380d139b10ef84f03
Milo C <75094742+whotf1@users.noreply.github.com>
2023-01-06 20:32:44 -0600

    added full basic functionality for the quiz

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,12 @@
 
 const startButton = document.getElementById('start-btn'); // initializing start button from html as startButton
+const nextButton = document.getElementById('next-btn'); // etc
 const questionContainerElement = document.getElementById('question-container') // initializing question container as questionContainerElement
 const questionElement = document.getElementById('question'); // initializing the question container for use as questionElement to write the questions to the html and their content from the questions array.
 const answerButtonsElement = document.getElementById('answer-buttons') // init answer-buttons as answerButtonsElement for writing the actual text to the buttons, in this case we need to grab the container and then can 'bubble down' the answers since they both exist in chronilogical order.
 
 
-let shuffledQuestions;
-let currentQuestionIndex;
+let shuffledQuestions, currentQuestionIndex
 
 
 
@@ -26,15 +26,58 @@ function startGame(){
 }
 
 function setNextQuestion(){
+  resetState();
   showQuestion(shuffledQuestions[currentQuestionIndex]); // calling showquestion with shuffledquestions at the current question index.
 }
 
 function showQuestion(question){
-  questionElement.innerText = question.question
+  questionElement.innerText = question.question; // populating the question field above the answer buttons, reading from question variable in question object from the currentquestionindex
+  question.answers.forEach(answer => { //arrow function just to compress all of this and do it efficiently 
+    const button = document.createElement('button'); // creating new buttons each time we call showquestions, essential for question seperation
+    button.innerText = answer.text; // setting new buttons text to the new questions answer text
+    button.classList.add('btn'); // setting new button to class of btn
+    if(answer.correct) {
+      button.dataset.correct = answer.correct; // setting only the correct data set to correct, and the rest can stay defaulted to false, this way we dont have to work with real bolean values, this will only happen if the button is correct
+    }
+    button.addEventListener('click', selectAnswer); //event listener as click and when we do we call select answer
+    answerButtonsElement.appendChild(button); // 'writing' or apending these new buttons we just made to the answer button section as children
+  })
 }
 
-function selectAnswer(){
 
+function resetState(){
+  clearStatusClass(document.body);
+  nextButton.classList.add('hide'); // hiding next button until we click on an answer\
+  while(answerButtonsElement.firstChild){// essential while, in answerButtonsElement if there is any first child inside of the answer button element
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild); // removing all answers that came before current, and next answers simply wont display
+  }
+}
+
+
+
+function selectAnswer(e) { // taking e(event) in as para
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct // getting the 'correct' data set that is true since it defualts to evaluating that way.
+  setStatusClass(document.body, correct); // selecting the body and telling it weather it should be set to correct or wrong.
+  // need to convert the live objects and datasets into an array, and running that for and through each of the buttons
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct); // setting the status of the button weather or not the status of the button is correct or not 
+  })
+}
+
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
 
 const questions = [ // questions is an array with objects, question is an element of an object ,answers is an array of objects, and those objects have 2 keywords with values, the questions with the content of the question and the value of correct to determine weather it is correct or not. 
@@ -46,5 +89,5 @@ const questions = [ // questions is an array with objects, question is an elemen
       {text: '239752', correct:false},
       {text: 'at least 7', correct:false},
     ]
-  }
+  },
 ]
