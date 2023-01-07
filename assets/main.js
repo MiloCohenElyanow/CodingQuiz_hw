fbd57016f199bbce1905b2e92c49f953883e0dbe
Milo C <75094742+whotf1@users.noreply.github.com>
2023-01-07 16:58:11 -0600

    added functioning timer internally

--- assets/main.js
+++ assets/main.js
@@ -1,40 +1,76 @@
 
 const startButton = document.getElementById('start-btn'); // initializing start button from html as startButton
 const nextButton = document.getElementById('next-btn'); // etc
-const questionContainerElement = document.getElementById('question-container') // initializing question container as questionContainerElement
+const questionContainerElement = document.getElementById('question-container'); // initializing question container as questionContainerElement
 const questionElement = document.getElementById('question'); // initializing the question container for use as questionElement to write the questions to the html and their content from the questions array.
-const answerButtonsElement = document.getElementById('answer-buttons') // init answer-buttons as answerButtonsElement for writing the actual text to the buttons, in this case we need to grab the container and then can 'bubble down' the answers since they both exist in chronilogical order.
+const answerButtonsElement = document.getElementById('answer-buttons'); // init answer-buttons as answerButtonsElement for writing the actual text to the buttons, in this case we need to grab the container and then can 'bubble down' the answers since they both exist in chronilogical order.
+const timer = document.getElementById('timer');
 
+let timerInt = 5;
 
 let shuffledQuestions, currentQuestionIndex
 
+var timerstart = setInterval(timerWrite, 1000);
+
+let l = 61
+function timerWrite(){
+  l--;
+  timer.textContent = l;
+
+  if (l < 1){
+    clearInterval(timerstart);
+  }
+
+}
 
 
-startButton.addEventListener('click', startGame); // listening when startButton is clicked and when it is we call startGame
+
+function startTimer(){
+  var timerInt = setInterval(writeTimer, 1000);
+  if(timerInt == 0){
+    clearInterval(timerInt);
+  }
+}
+
+function writeTimer(){
+    timer.textContent --;
+    timerInt --;
+    console.log(timer.textContent);
+}
+
+
+function afterTimer(){
+  timer.textContent = "OUT OF TIME";
+}
+
+startButton.addEventListener('click', startQ); // listening when startButton is clicked and when it is we call startGame
 nextButton.addEventListener('click',() =>{
   currentQuestionIndex++; // move current question index up one when next button is clicked and then set the next question with setnextquestion.
-  setNextQuestion();
+  NextQues();
 } )
 
 
-function startGame(){
+function startQ(){
   console.log("started game"); // just logging when the game is started to the console to make sure it did start
   startButton.classList.add('hide'); // when start button is clicked and startGame function is called we add startButton to the class list hide, and the start button become hidden
   shuffledQuestions = questions.sort(() => Math.random() - .5); // sorting question index randomly by using math random to sort it one way if its above 0 and a different way randomly 
   //if its under 0, we subtraced 0.5 from a number between 0 and 1 so it will always be between -1 and 1 giving a 50% chance to sort the questions 1 of 2 ways every single time the 
   //page is loaded. its less random than actually random but for this use case where somone would only take the quiz once I think this works just fine.
-  
+    
   currentQuestionIndex = 0; // setting index to 0 to start at the top after sorting
   questionContainerElement.classList.remove('hide'); // removing hide class from question container when user starts game
-  setNextQuestion(); // calling function setnextquestion which will set the next question to the screen from a array of objects that hold the questions and answers
+  timer.classList.remove('hide'); // removing hide element when quiz starts
+  /*timerAll();// calling timer when we remove hide from it, kind of starting the timer when its un-hidden */
+  NextQues(); // calling function setnextquestion which will set the next question to the screen from a array of objects that hold the questions and answers
+
 }
 
-function setNextQuestion(){
-  resetState();
-  showQuestion(shuffledQuestions[currentQuestionIndex]); // calling showquestion with shuffledquestions at the current question index.
+function NextQues(){
+  clearField();
+  showQues(shuffledQuestions[currentQuestionIndex]); // calling showquestion with shuffledquestions at the current question index.
 }
 
-function showQuestion(question){
+function showQues(question){
   questionElement.innerText = question.question; // populating the question field above the answer buttons, reading from question variable in question object from the currentquestionindex
   question.answers.forEach(answer => { //arrow function just to compress all of this and do it efficiently 
     const button = document.createElement('button'); // creating new buttons each time we call showquestions, essential for question seperation
@@ -43,15 +79,15 @@ function showQuestion(question){
     if(answer.correct) {
       button.dataset.correct = answer.correct; // setting only the correct data set to correct, and the rest can stay defaulted to false, this way we dont have to work with real bolean values, this will only happen if the button is correct
     }
-    button.addEventListener('click', selectAnswer); //event listener as click and when we do we call select answer
+    button.addEventListener('click', pickAnsw); //event listener as click and when we do we call select answer
     answerButtonsElement.appendChild(button); // 'writing' or apending these new buttons we just made to the answer button section as children
   })
 }
 
 
-function resetState(){
-  clearStatusClass(document.body); // reset the body background color to neutral when a answer for a question has not been selected.
-  clearStatusClass(document.body);
+function clearField(){
+  clearQField(document.body); // reset the body background color to neutral when a answer for a question has not been selected.
+  clearQField(document.body);
   nextButton.classList.add('hide'); // hiding next button until we click on an answer\
   while(answerButtonsElement.firstChild){// essential while, in answerButtonsElement if there is any first child inside of the answer button element
     answerButtonsElement.removeChild(answerButtonsElement.firstChild); // removing all answers that came before current, and next answers simply wont display
@@ -60,13 +96,13 @@ function resetState(){
 
 
 
-function selectAnswer(e) { // taking e(event) in as para
+function pickAnsw(e) { // taking e(event) in as para
   const selectedButton = e.target
   const correct = selectedButton.dataset.correct // getting the 'correct' data set that is true since it defualts to evaluating that way.
-  setStatusClass(document.body, correct); // selecting the body and telling it weather it should be set to correct or wrong.
+  setQType(document.body, correct); // selecting the body and telling it weather it should be set to correct or wrong.
   // need to convert the live objects and datasets into an array, and running that for and through each of the buttons
   Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct); // setting the status of the button weather or not the status of the button is correct or not 
+    setQType(button, button.dataset.correct); // setting the status of the button weather or not the status of the button is correct or not 
   })
   if(shuffledQuestions.length > currentQuestionIndex + 1){ // if we arent on the last question then we will show the next button otherwise the next button will not show. the next button return throws an error if there isnt any questions left.
   nextButton.classList.remove('hide'); // remove hide class from next button when select answer is called, or when the answer for the question is chosen.
@@ -78,8 +114,8 @@ function selectAnswer(e) { // taking e(event) in as para
 }
 
 
-function setStatusClass(element, correct) {
-  clearStatusClass(element)
+function setQType(element, correct) {
+  clearQField(element)
   if (correct) {
     element.classList.add('correct') // when the element(answer/choice box) results to being correct when clicked on we set the css class to correct and subsequently the wrest of them to wrong class
   } else {
@@ -87,7 +123,7 @@ function setStatusClass(element, correct) {
   }
 }
 
-function clearStatusClass(element) {
+function clearQField(element) {
   element.classList.remove('correct') // when moving to the next question we set the question boxes to neither correct or wrong to default to neutral class.
   element.classList.remove('wrong')
 }
