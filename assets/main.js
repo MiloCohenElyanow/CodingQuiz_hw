4907486e62793eb160906f32fdcdcea1d6c53f84
Milo C <75094742+whotf1@users.noreply.github.com>
2023-01-07 15:07:57 -0600

    added full question index, 15 questions

--- assets/main.js
+++ assets/main.js
@@ -102,4 +102,130 @@ const questions = [ // questions is an array with objects, question is an elemen
       {text: 'at least 7', correct:false},
     ]
   },
+  {
+    question: 'what is jQuery?',
+    answers: [
+      {text: 'A JS library designed to make Querying html and css elements easier and more effecient', correct: false},
+      {text: 'A HTML library made to make css design easier by putting a bunch of preset css elements into the html', correct: false},
+      {text: 'A JS library designed to simplify HTML DOM tree traversal, aswell as event handling, CSS animation, and Ajax', correct:true},
+      {text: 'A CSS library that makes design easier by adding lots of premade classes and elements and simply manipulating those', correct:false},
+    ]
+  },
+  {
+    question: 'what is 2*3?',
+    answers: [
+      {text: 'at least 4', correct: true},
+      {text: 'your mom', correct: false},
+      {text: '6', correct:true},
+      {text: '21...', correct:false},
+    ]
+  },
+  {
+    question: "what is Gary's favorite movie",
+    answers: [
+      {text: 'Star Trek', correct: false},
+      {text: 'Family Guy Movies', correct: false},
+      {text: 'StarWars franchise', correct:false},
+      {text: 'Doctor Strange', correct:true},
+    ]
+  },
+  {
+    question: 'from a famous vine quote...what are those?',
+    answers: [
+      {text: 'Chanclas', correct: true},
+      {text: "Nike Air Maxe's", correct: false},
+      {text: "Jordan Air Ones", correct:false},
+      {text: 'Crocks, in sport mode', correct:false},
+    ]
+  },
+  {
+    question: 'what is the most effecient form of sea travel?',
+    answers: [
+      {text: 'Nobody travelels the seas these days...', correct: false},
+      {text: 'Getting swallowed by a whale', correct: false},
+      {text: 'This question is dumb', correct:false},
+      {text: 'Traveling with Captain Jack Sparrow', correct:true},
+    ]
+  },
+  {
+    question: 'What is the best coding langauge, obviously un-biased',
+    answers: [
+      {text: 'Python', correct: true},
+      {text: 'HTML,CSS,JS', correct: true},
+      {text: 'C++,C#', correct:true},
+      {text: 'Ruby, Django', correct:true},
+    ]
+  },
+  {
+    question: 'If you were president what is the first thing you would HAVE to do',
+    answers: [
+      {text: 'Commit a Capitol crime, then pardon yourself', correct: true},
+      {text: 'Trash the whitehouse with a epic party', correct: true},
+      {text: 'Fill out paper work, because you just became president', correct:false},
+      {text: 'Tell your vice president your going on a 4 year vacation', correct:true},
+    ]
+  },
+  {
+    question: 'What is the most popular streaming platform',
+    answers: [
+      {text: 'HULU', correct: false},
+      {text: 'TWITCH', correct: true},
+      {text: 'NETFLIX', correct:false},
+      {text: 'YOUTUBE', correct:true},
+    ]
+  },
+  {
+    question: 'what is the best question answer?',
+    answers: [
+      {text: 'A', correct: false},
+      {text: 'B', correct: true},
+      {text: 'C', correct:false},
+      {text: 'D', correct:false},
+    ]
+  },
+  {
+    question: 'what is the most popular rythm game ever made',
+    answers: [
+      {text: 'OSU!', correct: true},
+      {text: 'Beat Saber', correct: false},
+      {text: 'Guitar Hero', correct:true},
+      {text: 'Friday Night Funkin', correct:false},
+    ]
+  },
+  {
+    question: 'what was a revolutionary 32-Bit MMO RPG game, released in 2001',
+    answers: [
+      {text: 'Minecraft', correct: false},
+      {text: 'Realm Of The Mad God', correct: false},
+      {text: 'RuneScape', correct:true},
+      {text: 'Impossible Quiz', correct:false},
+    ]
+  },
+  {
+    question: 'What company makes the best gaming periferals overall',
+    answers: [
+      {text: 'Logitech', correct: true},
+      {text: 'Glorious', correct: false},
+      {text: 'Turtle Beach', correct:false},
+      {text: 'Xbox', correct:false},
+    ]
+  },
+  {
+    question: 'What is bootstrap?',
+    answers: [
+      {text: 'A free open source css framework directed at mobile-first front end webdevelopement, containing templates, typography, buttons, navigation, and so much more', correct: true},
+      {text: 'ur mom lol', correct: true},
+      {text: 'Fortnight Battlepass', correct:true},
+      {text: 'something from Ohio', correct:false},
+    ]
+  },
+  {
+    question: 'what is 2+2?... But the second time',
+    answers: [
+      {text: '4', correct: false},
+      {text: 'bruh', correct: true},
+      {text: '239752', correct:false},
+      {text: 'at least 7', correct:false},
+    ]
+  },
 ]
